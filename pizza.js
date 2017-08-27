e9733e28867ed5379f8e4d0c347ea920d036d7bc
Toni <deea@seattleu.edu>
2017-08-27 14:11:10 -0700

    complete pizza generator

--- pizza.js
+++ pizza.js
@@ -1,10 +1,9 @@
 
-let pizzaSizes =
-    [
-        { size: 'small', price: 10 },
-        { size: 'medium', price: 13 },
-        { size: 'large', price: 20 }
-    ]
+let pizzaSizes = {
+    'Small': 10,
+    'Medium': 13,
+    'Large': 20
+}
 
 let pizzaBase = ['Tomato', 'Pesto', 'Alfredo']
 
@@ -38,31 +37,58 @@ let pizzaVegetable = new PizzaTopping(2, 1, [
 
 let pizzaMeat = new PizzaTopping(1, 3, ['Chicken', 'Pepperoni', 'Sausage'])
 
-function countBox(checkboxes){
-    let count = 0
-    for (let checkbox of checkboxes){
+function getSelectedBoxes(checkboxes) {
+    let checkedToppings = []
+    for (let checkbox of checkboxes) {
         if (checkbox.checked === true) {
-            count = count + 1
+            checkedToppings.push(checkbox.value)
         }
     }
-    return count 
+    return checkedToppings
 }
 
-function subtotal (){
-    let total = parseInt(document.querySelector('input[name=size]:checked').value);
-    
+function subtotal() {
+    let total = 0;
+    let selectedSize = document.querySelector('input[name=size]:checked').value;
+    total = total + pizzaSizes[selectedSize];
+
+   
+    let base = document.querySelectorAll('input[name=base]');
     let cheese = document.querySelectorAll('input[name=cheese]');
     let vegetables = document.querySelectorAll('input[name=vegetables]');
     let meat = document.querySelectorAll('input[name=meat]');
 
-    total = pizzaCheese.getTotal(countBox(cheese)) + total;
-    total = pizzaVegetable.getTotal(countBox(vegetables)) + total;
-    total = pizzaMeat.getTotal(countBox(meat)) + total;
-    
+   
+    let selectedBase = getSelectedBoxes(base);
+    let selectedCheese = getSelectedBoxes(cheese);
+    let selectedVegetables = getSelectedBoxes(vegetables);
+    let selectedMeat = getSelectedBoxes(meat);
+
+    total = pizzaCheese.getTotal(selectedCheese.length) + total;
+    total = pizzaVegetable.getTotal(selectedVegetables.length) + total;
+    total = pizzaMeat.getTotal(selectedMeat.length) + total;
+
     let totalDiv = document.querySelector('#total');
-    totalDiv.innerHTML = total
+    totalDiv.innerHTML = `$${total}`;
+
+    let sizeDiv = document.querySelector('#selected-size');
+    sizeDiv.innerHTML = selectedSize;
+
+    let baseDiv = document.querySelector('#selected-base');
+    baseDiv.innerHTML = selectedBase.join(', ');
+
+    let cheeseDiv = document.querySelector('#selected-cheese');
+    cheeseDiv.innerHTML = selectedCheese.join(', ');
+
+    let vegetableDiv = document.querySelector('#selected-vegetable');
+    vegetableDiv.innerHTML = selectedVegetables.join(', ');
+
+    let meatDiv = document.querySelector('#selected-meat');
+    meatDiv.innerHTML = selectedMeat.join(', ');
 
     return total
 }
 
 document.querySelector('#pizza-toppings').addEventListener('change', subtotal);
+
+subtotal()
